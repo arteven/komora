f5cbed9c15d1b76a899eebfbb2768e68e232bb6e
arteven <arteven@4r3k.cc>
2026-04-30 22:45:23 +0200

    feat(cli): komora ls

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { Command } from "commander";
 import { secretsSet, secretsList, secretsRm } from "./commands/secrets.js";
+import { ls } from "./commands/ls.js";
 
 const program = new Command();
 program.name("komora").description("Per-workspace microVM sandboxes for AI agents.").version("0.0.0");
@@ -9,5 +10,7 @@ const secrets = program.command("secrets").description("Manage the komora secret
 secrets.command("set <name>").option("--from-stdin", "read value from stdin").action((name, opts) => secretsSet(name, opts));
 secrets.command("list").action(() => secretsList());
 secrets.command("rm <name>").action((name) => secretsRm(name));
+
+program.command("ls").description("List sandboxes.").action(() => ls());
 
 program.parseAsync(process.argv);
